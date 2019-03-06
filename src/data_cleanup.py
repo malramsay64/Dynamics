c917b671239bb12a52547eb19014b13819f49857
Malcolm Ramsay <malramsay64@gmail.com>
2019-03-06 16:02:57 +1100

    feat: Update data cleanup script
    The data cleanup now includes a lot more comments explaining each step
    of the cleanup process. This will make it a lot easier to remove steps,
    since there is a clear reason for why they are there in the first place.

--- src/data_cleanup.py
+++ src/data_cleanup.py
@@ -18,11 +18,23 @@ from pathlib import Path
 
 
 def clean_hdf_file(filename: Path):
+    # Cleanup the dynamics dataset
     df = pandas.read_hdf(filename, "dynamics")
+
+    # Most of the values are plotted on a log scale for the time axis, values less than
+    # or equal to 0 cause issues.
+    df = df.query('time > 0')
+
+    # We want to discard values where there are not enough to get decent statistics, in
+    # this case I have chosen 10 as the magic number.
     df['count'] = df.groupby(['time', 'temperature', 'pressure'])['start_index'].transform('count')
     df = df.query('count > 10')
-    df = df.query('time > 0')
+    # Don't want the count in the final dataset, just a temporary column
     df = df.drop(columns=['count'], axis=1)
+
+    # The values where the MSD is greater than 100 are going to have issues with
+    # the periodic boundary conditions so remove those columns.
+    df = df.query('msd < 100')
 
     df.to_hdf(filename.with_name(filename.stem + "_clean" + ".h5"), "dynamics")
 
