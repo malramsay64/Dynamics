b4d7b9d16eeb54498be2194e5a0b2b074e00d4ec
Malcolm Ramsay <malramsay64@gmail.com>
2019-05-02 11:16:43 +1000

    feat: Create flag to specify the minimum number of samples
    When cleaning up, times for which there are not enough samples are
    discarded, however there this is not a fixed value, instead determined
    from what gives good results. This allows the minimum number of samples
    to be averaged over to be determined when running the analysis.

--- src/data_cleanup.py
+++ src/data_cleanup.py
@@ -15,11 +15,24 @@ the dynamics.
 import pandas
 import sys
 from pathlib import Path
+import click
 
 
-def clean_hdf_file(filename: Path):
+@click.command()
+@click.argument(
+    "infile", 
+    type=click.Path(file_okay=True, dir_okay=False, exists=True), 
+)
+@click.option(
+    "--min-samples",
+    default=10,
+    type=int,
+    help="Minimum number of samples for each data point.",
+)
+def clean_hdf_file(infile: Path, min_samples: int):
+    infile = Path(infile)
     # Cleanup the dynamics dataset
-    df = pandas.read_hdf(filename, "dynamics")
+    df = pandas.read_hdf(infile, "dynamics")
 
     # Most of the values are plotted on a log scale for the time axis, values less than
     # or equal to 0 cause issues.
@@ -28,7 +41,7 @@ def clean_hdf_file(filename: Path):
     # We want to discard values where there are not enough to get decent statistics, in
     # this case I have chosen 10 as the magic number.
     df = df.assign(count = df.groupby(['time', 'temperature', 'pressure'])['start_index'].transform('count'))
-    df = df.query('count > 10')
+    df = df.query('count > @min_samples')
     #  # Don't want the count in the final dataset, just a temporary column
     df = df.drop(columns=['count'], axis=1)
 
@@ -36,16 +49,10 @@ def clean_hdf_file(filename: Path):
     # the periodic boundary conditions so remove those columns.
     df = df.query('msd < 100')
 
-    df.to_hdf(filename.with_name(filename.stem + "_clean" + ".h5"), "dynamics")
+    df.to_hdf(infile.with_name(infile.stem + "_clean" + ".h5"), "dynamics")
 
-    df = pandas.read_hdf(filename, "molecular_relaxations")
-    df.to_hdf(filename.with_name(filename.stem + "_clean" + ".h5"), "molecular_relaxations")
+    df = pandas.read_hdf(infile, "molecular_relaxations")
+    df.to_hdf(infile.with_name(infile.stem + "_clean" + ".h5"), "molecular_relaxations")
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        filename = Path(sys.argv[1])
-        clean_hdf_file(filename)
-    else:
-        print("Requires input path for processing.")
-
-
+    clean_hdf_file()
