6d0ec077d1094aa3f2861af469422124557334a3
Malcolm Ramsay <malramsay64@gmail.com>
2019-05-02 14:04:02 +1000

    analysis: Add support for plotting static structure factor
    This adds the static structure factor (ssf) to the figures which can be
    created. This is another figure which I would like to be adding to my
    thesis.

--- src/figures.py
+++ src/figures.py
@@ -8,11 +8,19 @@
 
 """Code to assist in the creation of figures."""
 
+from typing import Optional
 import altair as alt
+import numpy as np
 import matplotlib.pyplot as plt
 import freud
 import sdanalysis
 import click
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# logging.basicConfig(level="DEBUG")
 
 
 def my_theme():
@@ -24,28 +32,93 @@ def use_my_theme():
     alt.themes.register("my_theme", my_theme)
     alt.themes.enable("my_theme")
 
-@click.command()
-@click.argument("infile", type=click.Path())
-@click.argument("outfile", type=click.Path())
-@click.option("--num-frames", default=10_000, type=int)
-def plot_radial_distribution(infile, outfile, num_frames) -> plt.Figure:
-    dr = 0.1
-    rmax = 20
-    rdf = freud.density.RDF(rmax=rmax, dr=dr)
 
+def radial_distribution(
+    infile,
+    dr: Optional[float] = None,
+    rmax: Optional[float] = None,
+    num_frames: int = 1000,
+) -> freud.density.RDF:
+    if rmax is None:
+        snapshot = next(sdanalysis.open_trajectory(infile))
+        box = snapshot.box
+        # 2D Box
+        if box[2] == 0.0:
+            rmax = np.min(box[:2]) / 2
+        # 3D box
+        else:
+            rmax = np.min(box[:3]) / 2
+        logger.debug("rmax set to None, calculated best value as %s", rmax)
+
+    if dr is None:
+        dr = rmax / 1000
+        logger.debug("dr set to None, calculated best value as %s", dr)
+
+    logger.debug("rmax: %f, dr: %f", rmax, dr)
+    rdf = freud.density.RDF(rmax=rmax, dr=dr)
     for index, snapshot in enumerate(sdanalysis.open_trajectory(infile)):
         if index > num_frames:
             break
         rdf.accumulate(snapshot.freud_box(), snapshot.position)
 
+    return rdf
+
+
+def _structure_factor_wave_number(
+    rdf: freud.density.RDF, wave_number: float, num_particles: int
+):
+    """Calculate the static structure factor for a single wave-number."""
+    dr = rdf.R[1] - rdf.R[0]
+    integral = dr * np.sum((rdf.RDF - 1) * rdf.R * np.sin(wave_number * rdf.R))
+    density = num_particles / rdf.box.volume
+    return 1 + 4 * np.pi * density / wave_number * integral
+
+
+def static_structure_factor(infile, num_frames):
+    num_particles = next(sdanalysis.open_trajectory(infile)).num_mols
+    rdf = radial_distribution(infile, num_frames=num_frames)
+
+    ssf = []
+    for value in rdf.R:
+        ssf.append(_structure_factor_wave_number(rdf, value, num_particles))
+
+    return rdf.R, ssf
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.argument("infile", type=click.Path())
+@click.argument("outfile", type=click.Path())
+@click.option("--num-frames", default=10000, type=int)
+def plot_rdf(infile, outfile, num_frames):
+    """Plot the radial distribution function of an input trajectory."""
+    rdf = radial_distribution(infile, num_frames=num_frames)
+
     fig, ax = plt.subplots()
     ax.plot(rdf.R, rdf.RDF)
-    ax.set_xlabel("r")
+    ax.set_xlabel(r"$r$")
     ax.set_ylabel("RDF")
-
     fig.savefig(outfile)
 
-    return fig
+
+@main.command()
+@click.argument("infile", type=click.Path())
+@click.argument("outfile", type=click.Path())
+@click.option("--num-frames", default=10000, type=int)
+def plot_ssf(infile, outfile, num_frames):
+    """Plot the static structure factor of an input trajectory."""
+    x, ssf = static_structure_factor(infile, num_frames)
+
+    fig, ax = plt.subplots()
+    ax.plot(x, ssf)
+    ax.set_xlabel(r"$k$")
+    ax.set_ylabel(r"$S(k)$")
+    fig.savefig(outfile)
+
 
 if __name__ == "__main__":
-    plot_radial_distribution()
+    main()
