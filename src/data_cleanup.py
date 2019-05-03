bb410e93e1776c3ffa9956a242f8c00cf85391b7
Malcolm Ramsay <malramsay64@gmail.com>
2019-05-03 19:01:40 +1000

    style: format data_cleanup.py using black
    This ensures the styling of the code is consistent with the rest of my
    code and additionally so I don't have to think about the style.

--- src/data_cleanup.py
+++ src/data_cleanup.py
@@ -19,10 +19,7 @@ import click
 
 
 @click.command()
-@click.argument(
-    "infile", 
-    type=click.Path(file_okay=True, dir_okay=False, exists=True), 
-)
+@click.argument("infile", type=click.Path(file_okay=True, dir_okay=False, exists=True))
 @click.option(
     "--min-samples",
     default=10,
@@ -36,23 +33,28 @@ def clean_hdf_file(infile: Path, min_samples: int):
 
     # Most of the values are plotted on a log scale for the time axis, values less than
     # or equal to 0 cause issues.
-    df = df.query('time > 0')
+    df = df.query("time > 0")
 
     # We want to discard values where there are not enough to get decent statistics, in
     # this case I have chosen 10 as the magic number.
-    df = df.assign(count = df.groupby(['time', 'temperature', 'pressure'])['start_index'].transform('count'))
-    df = df.query('count > @min_samples')
+    df = df.assign(
+        count=df.groupby(["time", "temperature", "pressure"])["start_index"].transform(
+            "count"
+        )
+    )
+    df = df.query("count > @min_samples")
     #  # Don't want the count in the final dataset, just a temporary column
-    df = df.drop(columns=['count'], axis=1)
+    df = df.drop(columns=["count"], axis=1)
 
     # The values where the MSD is greater than 100 are going to have issues with
     # the periodic boundary conditions so remove those columns.
-    df = df.query('msd < 100')
+    df = df.query("msd < 100")
 
     df.to_hdf(infile.with_name(infile.stem + "_clean" + ".h5"), "dynamics")
 
     df = pandas.read_hdf(infile, "molecular_relaxations")
     df.to_hdf(infile.with_name(infile.stem + "_clean" + ".h5"), "molecular_relaxations")
+
 
 if __name__ == "__main__":
     clean_hdf_file()
