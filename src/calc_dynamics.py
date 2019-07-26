fix: Fixes to the computation of normalised temperature
This ensures that the normalised temperature is calculated for both
arrays of string and floating point types.

src/calc_dynamics.py
@@ -43,14 +43,13 @@ def _upper(series: pd.Series):
 
 
 def normalised_temperature(temperature: np.array, pressure: np.array) -> np.array:
-    melting_points = {"13.50": 1.35, "1.00": 0.36}
+    melting_points = {13.50: 1.35, "13.50": 1.35, 1.00: 0.36, "1.00": 0.36}
     temp_norm = np.full_like(temperature, np.nan)
     # Temperature can't be zero or below, so set these values to nan
-    zero_mask = temperature <= 0
-    temp_norm[zero_mask] = np.nan
+    zero_mask = temperature > 0
+    temp_norm[~zero_mask] = np.nan
     for p, t_m in melting_points.items():
-        mask = np.logical_and(pressure == p, ~zero_mask)
-        print(mask)
+        mask = np.logical_and(pressure == p, zero_mask)
         temp_norm[mask] = t_m / temperature[mask]
 
     return temp_norm

test/calc_dynamics_test.py
@@ -13,7 +13,7 @@ from numpy.testing import assert_array_almost_equal
 from src import calc_dynamics
 
 
-@pytest.mark.parametrize("press, temp", [("13.50", 1.35), ("1.00", 0.36)])
+@pytest.mark.parametrize("press, temp", [("13.50", 1.35), ("1.00", 0.36) , (13.50, 1.35), (1.00, 0.36)])
 def test_temperature_normalisation_melting(press, temp):
     shape = 10
     pressure = np.full(shape, press)
@@ -22,7 +22,7 @@ def test_temperature_normalisation_melting(press, temp):
     assert_array_almost_equal(result, np.ones(shape))
 
 
-@pytest.mark.parametrize("press, temp", [("13.50", 1.35), ("1.00", 0.36)])
+@pytest.mark.parametrize("press, temp", [("13.50", 1.35), ("1.00", 0.36) , (13.50, 1.35), (1.00, 0.36)])
 def test_temperature_normalisation_one(press, temp):
     shape = 10
     pressure = np.full(shape, press)
@@ -31,7 +31,7 @@ def test_temperature_normalisation_one(press, temp):
     assert_array_almost_equal(result, np.full(shape, temp))
 
 
-@pytest.mark.parametrize("press, temp", [("13.50", 1.35), ("1.00", 0.36)])
+@pytest.mark.parametrize("press, temp", [("13.50", 1.35), ("1.00", 0.36) , (13.50, 1.35), (1.00, 0.36)])
 def test_temperature_normalisation_zero(press, temp):
     shape = 10
     pressure = np.full(shape, press)