1f5371e93705cc7d005058ae462a0f2e03a5bd24
Malcolm Ramsay <malramsay64@gmail.com>
2019-09-12 11:34:01 +1000

    analysis: Use melting point data file for normalisation
    This uses the data from the results/melting_point.csv file for the
    normalisation of the temperature. This means that the data for the
    melting points is in a single location and can easily be added to, or
    modified.
    
    Fixes #22

--- src/calc_dynamics.py
+++ src/calc_dynamics.py
@@ -46,15 +46,58 @@ def _upper(series: pd.Series):
         series.values, bs_stats.mean, alpha=0.1, num_iterations=1000
     ).upper_bound
 
+def _read_temperatures(filename: Path) -> Dict[float, float]:
+    """Read temperatures from a CSV file and format for simple translation.
+
+    Args:
+        filename: An input file which contains the melting points for each pressure.
+
+    """
+    df = pd.read_csv(infile)
+    melting_points = {}
+    for row in df:
+        melting_points[float(row["Pressure"])] = float(row["MeltingPoint"])
+    return melting_points
+
 
 def normalised_temperature(temperature: np.array, pressure: np.array) -> np.array:
-    melting_points = {13.50: 1.35, "13.50": 1.35, 1.00: 0.36, "1.00": 0.36}
+    """Calculate a normalised temperature based on the melting point.
+
+    This calculates normalised temperatures based on the melting points specified in the
+    'results/melting_points.csv' file.
+
+    Args:
+        temperature: A collection of temperatures for which the normalised 
+            value is to be calculated. These are all expected to be floating
+            point values.
+        pressure: The pressure corresponding to each of the input temperatures.
+            These are all expected to be floating point values.
+
+    Returns:
+        Array containing the temperatures normalised by the appropriate melting
+        point for the given pressure.
+
+    """
+    # Ensure input is floating point values.
+    if not isinstance(temperature, np.floating):
+        raise ValueError("The temperature needs to be specified as floating point values.")
+    if not isinstance(pressure, np.floating):
+        raise ValueError("The pressure needs to be specified as floating point values.")
+
+    melting_points = _read_temperatures(Path(__file__).parent / "results/melting_points.csv")
+
+    # Initialise output array
     temp_norm = np.full_like(temperature, np.nan)
+
     # Temperature can't be zero or below, so set these values to nan
     zero_mask = temperature > 0
     temp_norm[~zero_mask] = np.nan
+
+    # Iterate over all pressures in melting_points dictionary
     for p, t_m in melting_points.items():
+        # Find values with the same pressure
         mask = np.logical_and(pressure == p, zero_mask)
+        # Calculate normalised temperature
         temp_norm[mask] = t_m / temperature[mask]
 
     return temp_norm
