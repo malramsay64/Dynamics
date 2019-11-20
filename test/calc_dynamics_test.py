af5df612cdd93fff9ce2469e45a4c394907f51af
Malcolm Ramsay <malramsay64@gmail.com>
2019-11-20 14:21:49 +1100

    test: Update tests to make use of the module
    This updates the tests using the python module. The tests are now much
    easier to set up so I should create more of them.

--- test/calc_dynamics_test.py
+++ test/calc_dynamics_test.py
@@ -10,10 +10,10 @@ import numpy as np
 import pytest
 from numpy.testing import assert_array_almost_equal
 
-from src import calc_dynamics
+from dynamics_analysis import calc_dynamics
 
 
-@pytest.mark.parametrize("press, temp", [("13.50", 1.35), ("1.00", 0.36) , (13.50, 1.35), (1.00, 0.36)])
+@pytest.mark.parametrize("press, temp", [(13.50, 1.35), (1.00, 0.36)])
 def test_temperature_normalisation_melting(press, temp):
     shape = 10
     pressure = np.full(shape, press)
@@ -22,17 +22,26 @@ def test_temperature_normalisation_melting(press, temp):
     assert_array_almost_equal(result, np.ones(shape))
 
 
-@pytest.mark.parametrize("press, temp", [("13.50", 1.35), ("1.00", 0.36) , (13.50, 1.35), (1.00, 0.36)])
+@pytest.mark.parametrize("press, temp", [("13.50", 1.35), ("1.00", 0.36)])
+def test_temperature_normalisation_melting_fail(press, temp):
+    shape = 10
+    pressure = np.full(shape, press)
+    temperature = np.full(shape, temp)
+    with pytest.raises(ValueError):
+        result = calc_dynamics.normalised_temperature(temperature, pressure)
+
+
+@pytest.mark.parametrize("press, temp", [(13.50, 1.35), (1.00, 0.36)])
 def test_temperature_normalisation_one(press, temp):
     shape = 10
     pressure = np.full(shape, press)
-    temperature = np.ones(shape)
+    temperature = np.ones(shape) * temp
     result = calc_dynamics.normalised_temperature(temperature, pressure)
-    assert_array_almost_equal(result, np.full(shape, temp))
+    assert_array_almost_equal(result, np.full(shape, 1.0))
 
 
-@pytest.mark.parametrize("press, temp", [("13.50", 1.35), ("1.00", 0.36) , (13.50, 1.35), (1.00, 0.36)])
-def test_temperature_normalisation_zero(press, temp):
+@pytest.mark.parametrize("press", [13.50, 1.00])
+def test_temperature_normalisation_zero(press):
     shape = 10
     pressure = np.full(shape, press)
     temperature = np.zeros(shape)
